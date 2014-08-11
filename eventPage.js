4a2bb40ea58a620bddc2ebc95c1e8020fce874d6
Ahmed Kamal <a.abdelfatah@live.com>
2014-08-11 06:52:09 +0300

    fix cb problem

---
+++ eventPage.js
@@ -0,0 +1,114 @@
+    /**
+ * Created by akamal8 on 09/08/2014.
+ */
+
+
+//Global Variables
+var todolist;
+var itemcolor;
+
+window.onload= function () {
+    todolist = document.getElementById("todolist");
+    var currentInput = document.getElementsByClassName("noteinput")[0];
+    var currentcb = document.getElementsByClassName("notecb")[0];
+    var coloroptions = document.getElementsByClassName("coloroption");
+    currentInput.onkeydown = function (e) {
+        if(currentInput.value != "") {
+            OnEnterPressed(e);
+            }
+        }
+
+    currentInput.onmouseout = function (currentInput) {
+        //get the child index
+        var index =0;
+        var myparent = currentInput.parentNode;
+        while((myparent = myparent.previousSibling) != null){
+            index++;
+        }
+        //alert(index);
+        OnMouseOut(index);
+    }
+
+    currentcb.onclick = function (e) {
+        OnChecked(e.target);
+    }
+
+    for(var i =0; i<coloroptions.length; i++)
+    {
+        var coption = coloroptions.item(i);
+
+        coption.onclick = function(e){
+        var sender = e.target;
+        //TODO Add an effect on selected color
+        var todocbs = document.getElementsByClassName("notecb");
+        var targetcolor = window.getComputedStyle(sender , null).backgroundColor;
+         for(var j=0; j<todocbs.length; j++){
+             if(todocbs.item(j).checked){
+                 var myparent = todocbs.item(j).parentNode;
+                 myparent.style.backgroundColor = targetcolor;
+                 myparent.children[1].style.backgroundColor = targetcolor;
+                }
+            }
+        }
+    }
+}
+
+
+    function OnChecked (currentcb){
+
+        var myparent = currentcb.parentNode;
+        if(currentcb.checked == true){
+
+            myparent.children[1].style.textDecoration = "line-through";
+        }
+
+        if(currentcb.checked != true){
+
+            myparent.children[1].style.textDecoration = "none";
+        }
+
+    }
+
+
+function OnEnterPressed(e){
+    if(e.keyCode == 13) {
+        addNewClearItem();
+
+        var firstelementinput = todolist.children[0].children[1];
+        firstelementinput.focus();
+        firstelementinput.onkeydown = function (e) {
+            if(firstelementinput.value != "")
+            OnEnterPressed(e);
+        }
+    }
+}
+
+function OnMouseOut(itemId){
+    StorageArea.set({'id' : itemId });
+
+}
+
+function addNewClearItem() {
+
+    //create the container div
+    var newitem = document.createElement("div");
+    var cbinputelement = document.createElement("input");
+
+    //create the cb of the element
+    cbinputelement.type="checkbox";
+    cbinputelement.className = "notecb";
+    cbinputelement.onclick = function (e) {
+        OnChecked(e.target);
+    }
+    //create the input of the element
+    var textinputelement = document.createElement("input");
+    textinputelement.type = "text";
+    textinputelement.className = "noteinput";
+    textinputelement.autofocus = "";
+
+    newitem.appendChild(cbinputelement);
+    newitem.appendChild(textinputelement);
+
+    //insert the element in the html before other elements
+    todolist.insertBefore(newitem , todolist.children[0]);
+}
